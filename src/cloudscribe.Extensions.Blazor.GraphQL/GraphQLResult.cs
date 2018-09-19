9bd727e16af758f6b6c686925498fa291f0739db
Joe Audette <joe.audette@gmail.com>
2018-09-19 08:53:19 -0400

    tidy

--- src/cloudscribe.Extensions.Blazor.GraphQL/GraphQLResult.cs
+++ src/cloudscribe.Extensions.Blazor.GraphQL/GraphQLResult.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace cloudscribe.Extensions.Blazor.GraphQL
+﻿namespace cloudscribe.Extensions.Blazor.GraphQL
 {
     public class GraphQLResult<T> where T : class
     {
