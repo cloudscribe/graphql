3108b1e4d43a9d341c02b5cbc18af2a99bd71a96
Joe Audette <joe.audette@gmail.com>
2018-09-22 17:26:30 -0400

    use constants for graphql queries in blazor

---
+++ src/cloudscribe.Core.Blazor/GraphQLQueryConstants.cs
@@ -0,0 +1,62 @@
+﻿namespace cloudscribe.Core.Blazor
+{
+    public static class GraphQLQueryConstants
+    {
+        #region Queries
+
+        public const string SiteListQuery = @"
+            {
+              siteList {
+                id,
+                aliasId,
+                siteName,
+                siteFolderName
+              }
+            }";
+
+        public const string SingleSiteCompanyInfoQuery = @"
+                query MyQuery($siteId: ID!){
+                companyInfo: siteFromId (id: $siteId) {
+                id,
+                companyName,
+                companyCountry,
+                companyFax,
+                companyLocality,
+                companyPhone,
+                companyPostalCode,
+                companyPublicEmail,
+                companyWebsite,
+                companyRegion,
+                companyStreetAddress,
+                companyStreetAddress2
+                }
+            }";
+
+        #endregion
+
+        #region Mutations
+
+        public const string SiteCompanyInfoMutation = @"
+            mutation updateSiteCompanyInfo($siteId: ID!, $update: CompanyInfoUpdateInput!){
+                companyInfo: updateSiteCompanyInfo (id: $siteId, updateSiteCompanyInfo: $update) {
+                id,
+                companyName,
+                companyCountry,
+                companyFax,
+                companyLocality,
+                companyPhone,
+                companyPostalCode,
+                companyPublicEmail,
+                companyWebsite,
+                companyRegion,
+                companyStreetAddress,
+                companyStreetAddress2
+                }
+            }";
+
+        #endregion
+
+
+
+    }
+}
---
+++ src/cloudscribe.SimpleContent.Blazor/GraphQLQueryConstants.cs
@@ -0,0 +1,46 @@
+﻿namespace cloudscribe.SimpleContent.Blazor
+{
+    public static class GraphQLQueryConstants
+    {
+        #region PageQueries
+
+        public const string CmsPageQuery = @"
+            query MyQuery($projectId: String!, $slug: String!){
+                getPageFromSlug (projectId: $projectId, slug: $slug) {
+                    id,
+                    projectId,
+                    title,
+                    content,
+                    metaDescription,
+                    author,
+                    pubDate
+                  }
+            }";
+
+        #endregion
+
+        #region BlogQueries
+
+        public const string CmsPostListQuery = @"
+            query PostListQuery($projectId: String!, $category: String, $pageNumber : Int!, $pageSize : Int!){
+                postList (projectId: $projectId, category: $category, pageNumber: $pageNumber, pageSize: $pageSize) {
+                    pageNumber,
+                    pageSize,
+                    totalItems,
+                    data:posts {
+                      id,
+                      title,
+                      contentType,
+                      metaDescription,
+                      author,
+                      pubDate,
+                      content
+                      }
+                }
+            }";
+
+        #endregion
+
+
+    }
+}
